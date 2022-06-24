3422ed80e71c78acfdc626958ff2d039e7351531
Christian Suriano <cratea113@gmail.com>
2022-06-24 14:46:00 -0500

    updated dtabase and exomine/main.js

--- scripts/database.js
+++ scripts/database.js
@@ -29,11 +29,27 @@ const database = {
         { id: 5, mineralId: 2, facilityId: 3, quantity: 5  },
         { id: 6, mineralId: 3, facilityId: 3, quantity: 3  },
     ],
+    coloniesMinerals: [
+        { id: 1, mineralId: 1, colonyId: 1, quantity: 15 },
+        { id: 2, mineralId: 2, colonyId: 1, quantity: 6 },
+        { id: 3, mineralId: 3, colonyId: 2, quantity: 12 },
+        { id: 4, mineralId: 2, colonyId: 3, quantity: 1 }
+    ],
+    spaceCart: [
+        {
+            id: 1,
+            facilityId: 2,
+            mineralId: 2,
+            colonyId: 3,
+            quantity: 1
+
+        }
+    ],
 
     transientState: {}
 }
 
-export const getGovernor = () => {
+export const getGovernors = () => {
     return database.governors.map(governor => ({...governor}))
 }
 export const getColonies = () => {
@@ -48,6 +64,9 @@ export const getFacilities = () => {
 export const getFacilitiesMinerals = () => {
     return database.facilitiesMinerals.map(facilityMineral => ({...facilityMineral}))
 }
+export const getColoniesMinerals = () => {
+    return database.coloniesMinerals.map(colonyMineral => ({...colonyMineral}))
+}
 
 export const setFacility = (facilityId) => {
     database.transientState.selectedFacility = facilityId
@@ -56,8 +75,17 @@ export const setFacility = (facilityId) => {
 
 export const purchaseMineral = () => {
 
+    const newOrder = {...database.transientState}
+
+    const lastIndex = database.spaceCart.length - 1
+    newOrder.id = database.spaceCart[lastIndex].id + 1
+
+    newOrder.timestamp = Date.now()
+
+    database.spaceCart.push(newOrder)
+    
+    database.transientState = {}
         // Broadcast custom event to entire documement so that the
         // application can re-render and update state
         document.dispatchEvent( new CustomEvent("stateChanged") )
     }
-}
---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+import { exomine } from "./exomine.js"
+
+const mainContainer = document.querySelector("#container")
+
+const renderAllHTML = () => {
+    mainContainer.innerHTML = exomine()
+}
+
+renderAllHTML()
+
+document.addEventListener("stateChanged", event => {
+    console.log("State of data has changed. Regenerating HTML...")
+    renderAllHTML()
+})
